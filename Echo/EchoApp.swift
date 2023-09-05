dea32c2df9250e73a0505d523741e350b789ef72
EeshPara <eeshpara@gmail.com>
2023-09-05 17:46:50 -0500

    Initial Commit

---
+++ Echo/EchoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoApp.swift
+//  Echo
+//
+//  Created by Eeshwar Parasuramuni on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct EchoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
